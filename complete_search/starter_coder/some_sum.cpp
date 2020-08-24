69c6917c503f2b56a59ebeaf85e1bab7aa2fee10
thomas <thomasmckanna@gmail.com>
2020-08-23 21:16:42 -0500

    Added starter code for complete search.

---
+++ complete_search/starter_coder/some_sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // TODO: determine if "Either", "Even", or "Odd" based on n
+
+    return 0;
+}
---
+++ complete_search/starter_coder/some_sum.py
@@ -0,0 +1,3 @@
+n = int(input())
+
+# TODO: determine if "Either", "Even", or "Odd" based on n
